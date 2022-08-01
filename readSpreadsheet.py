a3b3764b985ee23e03f536018aee318ec7051f86
DCormican <dylan.cormican@poly.com>
2022-08-01 15:54:15 -0600

    added country codes, reformatted read spreadsheet

--- readSpreadsheet.py
+++ readSpreadsheet.py
@@ -2,8 +2,7 @@ from importlib.util import spec_from_loader
 import openpyxl
 
 class GetData:
-
-    def get_data():
+    def get_data(self):
         path = "spreadsheets\Licenses.xlsx"
 
         # To open the workbook
@@ -18,37 +17,44 @@ class GetData:
         m_col = 25
 
         myRows = []
+        uniqueCountries = []
         myInitials = "JR"
 
-        for i in range(1, m_row + 1):
+        for i in range(2, m_row + 1):
             
             rowVals = []
             cell_obj = sheet_obj.cell(row = i, column = 4)
 
-            if cell_obj.value == myInitials:
+            # if cell_obj.value == myInitials:
 
-                rowVals.append(sheet_obj.cell(row = i, column = 2).value) # customer name
-                rowVals.append(sheet_obj.cell(row = i, column = 12).value) # license number
-                rowVals.append(sheet_obj.cell(row = i, column = 19).value) # 2.0 part
-                rowVals.append(sheet_obj.cell(row = i, column = 20).value) # long description
-                rowVals.append(sheet_obj.cell(row = i, column = 13).value) # expiration date
-                rowVals.append(sheet_obj.cell(row = i, column = 10).value) # quantity
-                rowVals.append(sheet_obj.cell(row = i, column = 22).value) # city
-                rowVals.append(sheet_obj.cell(row = i, column = 23).value) # state
-                rowVals.append(sheet_obj.cell(row = i, column = 25).value) # zip code
-                rowVals.append(sheet_obj.cell(row = i, column = 26).value) # email address
-                rowVals.append(sheet_obj.cell(row = i, column = 27).value) # full name
+            rowVals.append(sheet_obj.cell(row = i, column = 2).value)  # customer name
+            rowVals.append(sheet_obj.cell(row = i, column = 12).value) # license number
+            rowVals.append(sheet_obj.cell(row = i, column = 19).value) # 2.0 part
+            rowVals.append(sheet_obj.cell(row = i, column = 20).value) # long description
+            rowVals.append(sheet_obj.cell(row = i, column = 13).value) # expiration date
+            rowVals.append(sheet_obj.cell(row = i, column = 10).value) # quantity
+            rowVals.append(sheet_obj.cell(row = i, column = 22).value) # city
+            rowVals.append(sheet_obj.cell(row = i, column = 23).value) # state
+            rowVals.append(sheet_obj.cell(row = i, column = 25).value) # zip code
+            rowVals.append(sheet_obj.cell(row = i, column = 26).value) # email address
+            rowVals.append(sheet_obj.cell(row = i, column = 27).value) # full name
+            rowVals.append(sheet_obj.cell(row = i, column = 24).value) # country
+            val = sheet_obj.cell(row = i, column = 24).value
+            if val not in uniqueCountries:
+                uniqueCountries.append(val)
 
-                myRows.append(rowVals)
-        
+            myRows.append(rowVals)
+
+        print(uniqueCountries)
         return myRows
         # for row in myRows:
         #     print(row)
 
+
 if __name__ == "__main__":
-    ss = GetData
+    ss = GetData()
 
     dr = ss.get_data()
 
-    for row in dr:
-        print(row)
+    # for row in dr:
+    #     print(row)
