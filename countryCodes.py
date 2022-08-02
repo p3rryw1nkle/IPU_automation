4c45cca7c88e5820b5e9f9fd38ee8e759ddff597
p3rryw1nkle <98891429+p3rryw1nkle@users.noreply.github.com>
2022-08-02 11:29:10 -0600

    more changes

--- countryCodes.py
+++ countryCodes.py
@@ -1,5 +1,4 @@
-country_codes = {'APAC': ['australia', 'isle of man', 'philippines', 'japan', 'ethiopia', 'belgium', 'taiwan', 'india', 'russian federation', 'china', 'korea, republic of', 'malaysia', 'hong kong', 'kazakhstan', 'new zealand', 'indonesia', 'singapore', 'viet nam'],
-                 'CALA': ['brazil', 'colombia', 'guatemala', 'chile', 'costa rica', 'argentina', 'mexico'],
-                 'EMEA': ['denmark', 'united kingdom', 'france', 'germany', 'italy', 'greece', 'sweden', 'spain', 'czech republic', 'slovakia', 'netherlands', 'norway', 'finland', 'switzerland', 'saudi arabia', 'burkina faso', 'south africa'],
-                 'NA':   ['north america', 'usa', 'united states of america', 'united states', 'canada']
+country_codes = {'370-3300-4010-63200': ['australia', 'isle of man', 'philippines', 'japan', 'ethiopia', 'belgium', 'taiwan', 'india', 'russian federation', 'china', 'korea, republic of', 'malaysia', 'hong kong', 'kazakhstan', 'new zealand', 'indonesia', 'singapore', 'viet nam'],
+                 '500-5000-4010-6320': ['denmark', 'united kingdom', 'france', 'germany', 'italy', 'greece', 'sweden', 'spain', 'czech republic', 'slovakia', 'netherlands', 'norway', 'finland', 'switzerland', 'saudi arabia', 'burkina faso', 'south africa'],
+                 '100-1000-4010-63200':   ['north america', 'usa', 'united states of america', 'united states', 'canada', 'brazil', 'colombia', 'guatemala', 'chile', 'costa rica', 'argentina', 'mexico']
                  }
